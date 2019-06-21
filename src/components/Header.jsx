0dd1af82fe218fca694f89a8815fb716c019f08c
TheWho16 <49193238+TheWho16@users.noreply.github.com>
2019-06-21 17:28:20 +0200

    BudCamp_1

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './css/Header.css'
+import Logo from './foto/Logo.PNG';
+
+const Header = () => {
+    return <header className='header'>
+        <img src={Logo} />
+    </header>
+
+}
+
+export default Header; 
